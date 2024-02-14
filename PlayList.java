93426aa124115d25f2f424b9409955bf1f825705
MayaBenHarush <maya.benharush@post.runi.ac.il>
2024-02-14 22:58:36 +0200

    Maya Benharush hw 08

--- PlayList.java
+++ PlayList.java
@@ -175,10 +175,10 @@ class PlayList {
      */
     private int minIndex(int start) {
 
-        if( start>=0 && start< size){
+        if( start>=0 && start< size && size > 0 ){
             int min= tracks[start].getDuration();
             int index = start;
-            for( int i = start+1; i< size; i++){
+            for( int i = start + 1; i < size; i++){
                 if(tracks[i].getDuration()< min){
                     min= tracks[i].getDuration();
                     index= i;
